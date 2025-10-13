024d58e69c9ef4422f14ef4b10972d81f2af1888
TACO3141 <andrewej10@gmail.com>
2025-10-13 16:26:17 -0600

    Work on string array reversal method

--- ArrayComps.java
+++ ArrayComps.java
@@ -2,6 +2,18 @@ public class ArrayComps
 {
   public static reverseString(String[] arr)
   {
-    // To add!! lol am lazy rn
+    if (arr.length <= 0)
+    {
+      throw ArrayIndexOutOfBoundsException("Array has no elements.");
+    }
+    else if (arr.length == 1)
+    {
+      return arr;
+    }
+    else
+    {
+      int arrayMaxIndex = arr.length - 1;
+      // To finish
+    }
   }
 }
