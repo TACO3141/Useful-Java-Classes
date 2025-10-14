83d6c3b9e92612bfe4966803cacd511ef4119602
TACO3141 <andrewej10@gmail.com>
2025-10-14 12:30:41 -0600

    Added some primitive type array reversals
    int, double, char and boolean

--- ArrayComps.java
+++ ArrayComps.java
@@ -1,6 +1,114 @@
 public class ArrayComps
 {
-  public static String[] reverseString(String[] arr)
+  public static String[] reverseStringArray(String[] arr)
+  {
+    if (arr.length <= 0)
+    {
+      throw new ArrayIndexOutOfBoundsException("Array has no elements.");
+    }
+    else if (arr.length == 1)
+    {
+      return arr;
+    }
+    else
+    {
+      int highIndex = arr.length - 1;
+      int lowIndex = 0;
+      String temporary = "";
+      while (lowIndex < highIndex)
+      {
+        temporary = arr[highIndex];
+        arr[highIndex] = arr[lowIndex];
+        arr[lowIndex] = temporary;
+        highIndex --;
+        lowIndex ++;
+      }
+      return arr;
+    }
+  }
+
+  public static int[] reverseIntArray(int[] arr)
+  {
+    if (arr.length <= 0)
+    {
+      throw new ArrayIndexOutOfBoundsException("Array has no elements.");
+    }
+    else if (arr.length == 1)
+    {
+      return arr;
+    }
+    else
+    {
+      int highIndex = arr.length - 1;
+      int lowIndex = 0;
+      String temporary = "";
+      while (lowIndex < highIndex)
+      {
+        temporary = arr[highIndex];
+        arr[highIndex] = arr[lowIndex];
+        arr[lowIndex] = temporary;
+        highIndex --;
+        lowIndex ++;
+      }
+      return arr;
+    }
+  }
+
+  public static double[] reverseDoubleArray(double[] arr)
+  {
+    if (arr.length <= 0)
+    {
+      throw new ArrayIndexOutOfBoundsException("Array has no elements.");
+    }
+    else if (arr.length == 1)
+    {
+      return arr;
+    }
+    else
+    {
+      int highIndex = arr.length - 1;
+      int lowIndex = 0;
+      String temporary = "";
+      while (lowIndex < highIndex)
+      {
+        temporary = arr[highIndex];
+        arr[highIndex] = arr[lowIndex];
+        arr[lowIndex] = temporary;
+        highIndex --;
+        lowIndex ++;
+      }
+      return arr;
+    }
+  }
+
+  public static char[] reverseCharArray(char[] arr)
+  {
+    if (arr.length <= 0)
+    {
+      throw new ArrayIndexOutOfBoundsException("Array has no elements.");
+    }
+    else if (arr.length == 1)
+    {
+      return arr;
+    }
+    else
+    {
+      int highIndex = arr.length - 1;
+      int lowIndex = 0;
+      String temporary = "";
+      while (lowIndex < highIndex)
+      {
+        temporary = arr[highIndex];
+        arr[highIndex] = arr[lowIndex];
+        arr[lowIndex] = temporary;
+        highIndex --;
+        lowIndex ++;
+      }
+      return arr;
+    }
+  }
+
+  public static boolean[] reverseBooleanArray(boolean[] arr)
   {
     if (arr.length <= 0)
     {
